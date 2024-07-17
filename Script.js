a7a0954d864b3163dd229004f82cd51214713fc3
marcossonicteccursos <132494370+marcossonicteccursos@users.noreply.github.com>
2024-07-17 16:03:50 -0400

    Script.js

--- Script.js
+++ Script.js
@@ -1,22 +1,20 @@
-        function repeatimg() {
+function repeatimg() {
+    setTimeout(function() {
+        document.getElementById('img1').style.display = 'none';
+        document.getElementById('img2').style.display = 'block';
+        setTimeout(function() {
+            document.getElementById('img2').style.display = 'none';
+            document.getElementById('img3').style.display = 'block';
             setTimeout(function() {
-                document.getElementById('img1').style.display = 'none';
-                document.getElementById('img2').style.display = 'block';
+                document.getElementById('img3').style.display = 'none';
+                document.getElementById('img4').style.display = 'block';
                 setTimeout(function() {
-                    document.getElementById('img2').style.display = 'none';
-                    document.getElementById('img3').style.display = 'block';
-                    setTimeout(function() {
-                        document.getElementById('img3').style.display = 'none';
-                        document.getElementById('img4').style.display = 'block';
-                        setTimeout(function() {
-                            document.getElementById('img4').style.display = 'none';
-                            document.getElementById('img1').style.display = 'block';
-                            // Chamar repeatimg() novamente após um ciclo completo
-                            repeatimg();
-                        }, 2000); // 1000 milissegundos = 1 segundo
-                    }, 2000); // 1000 milissegundos = 1 segundo
+                    document.getElementById('img4').style.display = 'none';
+                    document.getElementById('img1').style.display = 'block';
+                    // Chamar repeatimg() novamente após um ciclo completo
+                    repeatimg();
                 }, 2000); // 1000 milissegundos = 1 segundo
-            }, 2000); // 1000 milissegundos = 1 segundo            
-        }
-
-        repeatimg();
+            }, 2000); // 1000 milissegundos = 1 segundo
+        }, 2000); // 1000 milissegundos = 1 segundo
+    }, 2000); // 1000 milissegundos = 1 segundo
+}

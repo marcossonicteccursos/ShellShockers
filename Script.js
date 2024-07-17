bcc3f10f3a58168540e891f368a417cc8ab06c3f
marcossonicteccursos <132494370+marcossonicteccursos@users.noreply.github.com>
2024-07-17 13:40:36 -0400

    Script.js

---
+++ Script.js
@@ -0,0 +1,22 @@
+        function repeatimg() {
+            setTimeout(function() {
+                document.getElementById('img1').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+                setTimeout(function() {
+                    document.getElementById('img2').style.display = 'none';
+                    document.getElementById('img3').style.display = 'block';
+                    setTimeout(function() {
+                        document.getElementById('img3').style.display = 'none';
+                        document.getElementById('img4').style.display = 'block';
+                        setTimeout(function() {
+                            document.getElementById('img4').style.display = 'none';
+                            document.getElementById('img1').style.display = 'block';
+                            // Chamar repeatimg() novamente após um ciclo completo
+                            repeatimg();
+                        }, 2000); // 1000 milissegundos = 1 segundo
+                    }, 2000); // 1000 milissegundos = 1 segundo
+                }, 2000); // 1000 milissegundos = 1 segundo
+            }, 2000); // 1000 milissegundos = 1 segundo            
+        }
+
+        repeatimg();
